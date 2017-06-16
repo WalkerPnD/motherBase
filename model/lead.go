162e847c90dc9fe8e2b3b411e320c71c0c12c00b
Koji <stylethewalker@gmail.com>
2017-06-15 22:55:16 -0300

    update: performance tuning

--- model/lead.go
+++ model/lead.go
@@ -2,14 +2,14 @@ package model
 
 // Lead is a potential sales contact
 type Lead struct {
-	CompanyName string `csv:"Company Name" gorm:"type:varchar(63)"`
-	FirstName   string `csv:"First Name" gorm:"type:varchar(127)"`
+	CompanyName string `csv:"Company Name" gorm:"type:varchar(63);index"`
+	FirstName   string `csv:"First Name" gorm:"type:varchar(127);index"`
 	LastName    string `csv:"Last Name" gorm:"type:varchar(127)"`
-	JobTitle    string `csv:"Job Title" gorm:"type:varchar(63)"`
+	JobTitle    string `csv:"Job Title" gorm:"type:varchar(63);index"`
 	City        string `csv:"City" gorm:"type:varchar(127)"`
 	LinkedIn    string `csv:"Linkedin" gorm:"primary_key" gorm:"type:varchar(511)"`
-	Industry    string `csv:"Industry" gorm:"type:varchar(63)"`
-	Email       string `csv:"Email" gorm:"type:varchar(127)"`
-	Sheets      string `csv:"Nome Da Planilha" gorm:"type:varchar(63)"`
+	Industry    string `csv:"Industry" gorm:"type:varchar(63);index"`
+	Email       string `csv:"Email" gorm:"type:varchar(127);index"`
+	Sheets      string `csv:"Nome Da Planilha" gorm:"type:varchar(63);index"`
 	HardBounce  bool   `csv:"Hardbounce"`
 }
